4c7d9b892a584a4239889c5dac940300c7d0c6c1
Sammmmmuel <69543188+Sammmmmuel@users.noreply.github.com>
2023-12-07 03:24:22 -0800

    working in terminal

--- app.py
+++ app.py
@@ -1,70 +1,3 @@
-# from flask import *
-# app = Flask(__name__)
-
-# @app.route("/", methods=["GET", "POST"])
-# def home():
-#     if request.method == "POST":
-#         print(request.form["name"])
-#         print(request.form["email"])
-#         return
-
-#     return render_template("index.html")
-# if __name__ == "__main__":
-#     app.run()
-# class TrieNode:
- 
-#     #constructor, Time O(1) Space O(1)
-#     def __init__(self, c):
-#         self.data = c
-#         self.isEnd = False
-#         self.children = {} #map
- 
-# class Trie:
-#     #constructor, Time O(1) Space O(1)
-#     def __init__(self):
-#         self.root = TrieNode('')
-    
-#     #Add a word to trie, Time O(s) Space O(1) s is word length
-#     def insert(self, word):
-#         node = self.root
-#         for ch in word:
-#             if not ch in node.children:
-#                 node.children[ch] = TrieNode(ch)   
-#             node = node.children[ch]                  
-#         node.isEnd = True
-   
-#     #find all word with given prefix
-# 	#Time O(n), Space O(n), n is number of nodes involved (include prefix and branches)
-#     def autocomplete(self, word):
-#         res = []
-#         node = self.root
-#         for ch in word:           
-#             if ch in node.children:
-#                 node = node.children[ch]
-#             else:
-#                 return []
-#         self.helper(node, res, word[:-1]) #except the last "ama", node is "z"
-#         return res
- 
-#     #recursive function called by autocomplete
-# 	#Time O(n), Space O(n), n is number of nodes in branches
-#     def helper(self, node, res, prefix):
-#         if node.isEnd:
-#             res.append(prefix + node.data)       
-#         for child in node.children.values():
-#             self.helper(child, res, prefix + node.data)
- 
-# t = Trie()      
-# pokemon = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise","Caterpie","Metapod","Butterfree","Weedle","Kakuna","Beedrill","Pidgey","Pidgeotto","Pidgeot","Rattata","Raticate","Spearow","Fearow","Ekans","Arbok","Pikachu","Raichu","Sandshrew","Sandslash","Nidoran","Nidorina","Nidoqueen","Nidoran","Nidorino","Nidoking","Clefairy","Clefable","Vulpix","Ninetales","Jigglypuff","Wigglytuff","Zubat","Golbat","Oddish","Gloom","Vileplume","Paras","Parasect","Venonat","Venomoth","Diglett","Dugtrio","Meowth","Persian","Psyduck","Golduck","Mankey","Primeape","Growlithe","Arcanine","Poliwag","Poliwhirl","Poliwrath","Abra","Kadabra","Alakazam","Machop","Machoke","Machamp","Bellsprout","Weepinbell","Victreebel","Tentacool","Tentacruel","Geodude","Graveler","Golem","Ponyta","Rapidash","Slowpoke","Slowbro","Magnemite","Magneton","Farfetch'd","Doduo","Dodrio","Seel","Dewgong","Grimer","Muk","Shellder","Cloyster","Gastly","Haunter","Gengar","Onix","Drowzee","Hypno","Krabby","Kingler","Voltorb","Electrode","Exeggcute","Exeggutor","Cubone","Marowak","Hitmonlee","Hitmonchan","Lickitung","Koffing","Weezing","Rhyhorn","Rhydon","Chansey","Tangela","Kangaskhan","Horsea","Seadra","Goldeen","Seaking","Staryu","Starmie","Mr. Mime","Scyther","Jynx","Electabuzz","Magmar","Pinsir","Tauros","Magikarp","Gyarados","Lapras","Ditto","Eevee","Vaporeon","Jolteon","Flareon","Porygon","Omanyte","Omastar","Kabuto","Kabutops","Aerodactyl","Snorlax","Articuno","Zapdos","Moltres","Dratini","Dragonair","Dragonite","Mewtwo","Mew"]
-# t.insert("Bublbasaur")
-# t.insert("Ivysaur")
-# t.insert("Venusaur")
-# t.insert("Charmander")
-# t.insert("Charmeleon")
-# t.insert("Charizard")
-# a = t.autocomplete("Char")
-# print(a)
-
 
 from flask import Flask, request, render_template 
  
@@ -75,62 +8,94 @@ app = Flask(__name__)
 # which URL is associated function
 @app.route('/', methods =["GET", "POST"])
 def gfg():
+    comp = []
     if request.method == "POST":
-        class TrieNode:
- 
-#     #constructor, Time O(1) Space O(1)
-            def __init__(self, c):
-                self.data = c
-                self.isEnd = False
-                self.children = {} #map
- 
-        class Trie:
-    #constructor, Time O(1) Space O(1)
+        class TrieNode():
             def __init__(self):
-                self.root = TrieNode('')
-    
-    #Add a word to trie, Time O(s) Space O(1) s is word length
-        def insert(self, word):
-            node = self.root
-            for ch in word:
-                if not ch in node.children:
-                    node.children[ch] = TrieNode(ch)   
-                node = node.children[ch]                  
-            node.isEnd = True
-   
-    #find all word with given prefix
-	#Time O(n), Space O(n), n is number of nodes involved (include prefix and branches)
-        def autocomplete(self, word):
-            res = []
-            node = self.root
-            for ch in word:           
-                if ch in node.children:
-                    node = node.children[ch]
-                else:
-                    return []
-            self.helper(node, res, word[:-1]) #except the last "ama", node is "z"
-            return res
+        # Initialising one node for trie
+                self.children = {}
+                self.last = False
  
-    #recursive function called by autocomplete
-	#Time O(n), Space O(n), n is number of nodes in branches
-        def helper(self, node, res, prefix):
-            if node.isEnd:
-                res.append(prefix + node.data)       
-            for child in node.children.values():
-                self.helper(child, res, prefix + node.data)
  
-        t = Trie()      
-        pokemon = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle","Wartortle","Blastoise","Caterpie","Metapod","Butterfree","Weedle","Kakuna","Beedrill","Pidgey","Pidgeotto","Pidgeot","Rattata","Raticate","Spearow","Fearow","Ekans","Arbok","Pikachu","Raichu","Sandshrew","Sandslash","Nidoran","Nidorina","Nidoqueen","Nidoran","Nidorino","Nidoking","Clefairy","Clefable","Vulpix","Ninetales","Jigglypuff","Wigglytuff","Zubat","Golbat","Oddish","Gloom","Vileplume","Paras","Parasect","Venonat","Venomoth","Diglett","Dugtrio","Meowth","Persian","Psyduck","Golduck","Mankey","Primeape","Growlithe","Arcanine","Poliwag","Poliwhirl","Poliwrath","Abra","Kadabra","Alakazam","Machop","Machoke","Machamp","Bellsprout","Weepinbell","Victreebel","Tentacool","Tentacruel","Geodude","Graveler","Golem","Ponyta","Rapidash","Slowpoke","Slowbro","Magnemite","Magneton","Farfetch'd","Doduo","Dodrio","Seel","Dewgong","Grimer","Muk","Shellder","Cloyster","Gastly","Haunter","Gengar","Onix","Drowzee","Hypno","Krabby","Kingler","Voltorb","Electrode","Exeggcute","Exeggutor","Cubone","Marowak","Hitmonlee","Hitmonchan","Lickitung","Koffing","Weezing","Rhyhorn","Rhydon","Chansey","Tangela","Kangaskhan","Horsea","Seadra","Goldeen","Seaking","Staryu","Starmie","Mr. Mime","Scyther","Jynx","Electabuzz","Magmar","Pinsir","Tauros","Magikarp","Gyarados","Lapras","Ditto","Eevee","Vaporeon","Jolteon","Flareon","Porygon","Omanyte","Omastar","Kabuto","Kabutops","Aerodactyl","Snorlax","Articuno","Zapdos","Moltres","Dratini","Dragonair","Dragonite","Mewtwo","Mew"]
-        t.insert("Bublbasaur")
-        t.insert("Ivysaur")
-        t.insert("Venusaur")
-        t.insert("Charmander")
-        t.insert("Charmeleon")
-        t.insert("Charizard")
-        a = t.autocomplete(request.form.get("pokemon"))
-        # print(a)
-        # a = request.form.get("pokemon")
-        return "the words that end in that are"+ a
+        class Trie():
+            def __init__(self):
+ 
+        # Initialising the trie structure.
+                self.root = TrieNode()
+ 
+            def formTrie(self, keys):
+ 
+        # Forms a trie structure with the given set of strings
+        # if it does not exists already else it merges the key
+        # into it by extending the structure as required
+                    for key in keys:
+                        self.insert(key)  # inserting one key to the trie.
+ 
+            def insert(self, key):
+ 
+        # Inserts a key into trie if it does not exist already.
+        # And if the key is a prefix of the trie node, just
+        # marks it as leaf node.
+                node = self.root
+ 
+                for a in key:
+                    if not node.children.get(a):
+                            node.children[a] = TrieNode()
+ 
+                    node = node.children[a]
+ 
+                node.last = True
+ 
+            def suggestionsRec(self, node, word):
+ 
+        # Method to recursively traverse the trie
+        # and return a whole word.
+                if node.last:
+                    print(word)
+ 
+                for a, n in node.children.items():
+                    self.suggestionsRec(n, word + a)
+ 
+            def printAutoSuggestions(self, key):
+ 
+        # Returns all the words in the trie whose common
+        # prefix is the given key thus listing out all
+        # the suggestions for autocomplete.
+                node = self.root
+ 
+                for a in key:
+            # no string in the Trie has this prefix
+                    if not node.children.get(a):
+                        return 0
+                    node = node.children[a]
+ 
+        # If prefix is present as a word, but
+        # there is no subtree below the last
+        # matching node.
+                if not node.children:
+                    return -1
+ 
+                self.suggestionsRec(node, key)
+                return 1
+ 
+ 
+# Driver Code
+        keys = ["charmander", "charmelion", "charizard", "cat", "a",
+        "hel", "help", "helps", "helping"]  # keys to form the trie structure.
+        key = request.form.get("pokemon")  # key for autocomplete suggestions.
+ 
+# creating trie object
+        t = Trie()
+ 
+# creating the trie structure with the
+# given set of strings.
+        t.formTrie(keys)
+ 
+# autocompleting the given key using
+# our trie structure.
+        comp = t.printAutoSuggestions(key)
+        word = str(comp)
+        return word
     return render_template("index.html")
  
 if __name__=='__main__':
